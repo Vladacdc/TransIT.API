8a040e41142e7768e0e140b6414b40cfbeba09d9
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-07-22 17:26:12 +0300

    Fix connection string

--- TransIT.DAL/Models/TransITDBContext.cs
+++ TransIT.DAL/Models/TransITDBContext.cs
@@ -34,7 +34,7 @@ namespace TransIT.DAL.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TransITDB_test;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TransITDB;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
     
         protected override void OnModelCreating(ModelBuilder modelBuilder)
