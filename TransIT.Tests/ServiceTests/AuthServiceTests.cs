de5fbf85cf909148ae16b1cac5222dd2be18c185
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-07-31 23:21:29 +0300

    Make required changes

--- TransIT.Tests/ServiceTests/AuthServiceTests.cs
+++ TransIT.Tests/ServiceTests/AuthServiceTests.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Test;
-using Moq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Test;
+using Moq;
 using TransIT.BLL.DTOs;
 using TransIT.BLL.Helpers.Abstractions;
 using TransIT.BLL.Services.ImplementedServices;
@@ -16,18 +16,92 @@ using Xunit;
 
 namespace TransIT.Tests
 {
-    public partial class AuthServiceTests
+    public class AuthServiceTests
     {
-        private Mock<IUnitOfWork> _unitOfWork;
-        private Mock<IJwtFactory> _factory;
-
         private readonly TokenDTO _testToken = new BLL.DTOs.TokenDTO()
         {
             AccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6InRlc3RBZG1pbiIsInJvbGUiOiJBRE1JTiIsInN1YiI6Ijk3NmY4NTQyLWNlZjItNDRhOS1iY2NkLWRlNzE0NDhhNzkwNyIsImp0aSI6ImIyZjBmNmRhLTY0ZDMtNDZhMi1hYTZmLTk4NGNlZTZjMTc4ZSIsImlhdCI6MTU2NDU4MzYyMSwibmJmIjoxNTY0NTgzNjIwLCJleHAiOjE1NjQ1ODcyMjAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCJ9.paX4-N6OqFpM_gCeRlOoQGUTVoJT00mOttC6ubDlMOQ",
             RefreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6InRlc3RBZG1pbiIsInJvbGUiOiJBRE1JTiIsInN1YiI6Ijk3NmY4NTQyLWNlZjItNDRhOS1iY2NkLWRlNzE0NDhhNzkwNyIsImp0aSI6ImE4ODBjYzA1LTJjYjYtNGMwZS04OTEyLTgzNjIxZjZjYmM1OSIsImlhdCI6MTU2NDU4MzYyMSwibmJmIjoxNTY0NTgzNjIwLCJleHAiOjE1NjUxODg0MjAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCJ9.SKASB4jrHS0_whmhmHKPFnYxgVJEyCHtQx2NaR-9lGU"
         };
 
+        private Mock<IUnitOfWork> _unitOfWork;
+        private Mock<IJwtFactory> _factory;
+
         public AuthServiceTests()
+        {
+            InitializeJwtFactory();
+            InitializeUnitOfWork();
+        }
+
+        [Fact]
+        public async Task AuthService_Should_Authorize_With_Valid_Userdata()
+        {
+            // Arrange
+            var service = new AuthenticationService(
+                MockHelpers.MockILogger<AuthenticationService>().Object,
+                _factory.Object,
+                _unitOfWork.Object
+            );
+
+            // Act
+            var result = await service.SignInAsync(
+                new BLL.DTOs.LoginDTO()
+                {
+                    Login = "bbbbc",
+                    Password = "HelloWord123@"
+                });
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task AuthService_Should_Not_Authorize_With_Invalid_Userdata()
+        {
+            // Arrange
+            var service = new AuthenticationService(
+                MockHelpers.MockILogger<AuthenticationService>().Object,
+                _factory.Object,
+                _unitOfWork.Object
+            );
+
+            // Act
+            var result = await service.SignInAsync(
+                new BLL.DTOs.LoginDTO()
+                {
+                    Login = "bbbbc",
+                    Password = "HellsssssoWord123@"
+                });
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task AuthService_Should_Refresh_Token()
+        {
+            // Arrange
+            var service = new AuthenticationService(
+                MockHelpers.MockILogger<AuthenticationService>().Object,
+                _factory.Object,
+                _unitOfWork.Object
+            );
+
+            // Act
+            var result = await service.SignInAsync(
+                new BLL.DTOs.LoginDTO()
+                {
+                    Login = "bbbbc",
+                    Password = "HelloWord123@"
+                });
+
+            var refreshResult = await service.TokenAsync(result);
+
+            // Assert
+            Assert.NotNull(refreshResult);
+        }
+
+        private void InitializeJwtFactory()
         {
             _factory = new Mock<IJwtFactory>();
             _factory.Setup(f => f.GenerateToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
@@ -45,12 +119,15 @@ namespace TransIT.Tests
                         new JwtSecurityToken(_testToken.RefreshToken)
                     );
                 });
+        }
 
+        private void InitializeUnitOfWork()
+        {
             _unitOfWork = new Mock<IUnitOfWork>();
             _unitOfWork.Setup(u => u.UserManager)
                 .Returns(() =>
                 {
-                    Mock<UserManager<User>> mock = MockHelpers.MockUserManager<User>();
+                    var mock = MockHelpers.MockUserManager<User>();
                     mock.Setup(m => m.FindByNameAsync(It.IsNotNull<string>()))
                         .Returns((string name) =>
                         {
@@ -95,60 +172,6 @@ namespace TransIT.Tests
                             }));
                     return mock.Object;
                 });
-        }
-
-        [Fact]
-        public async Task AuthService_Should_Authorize_With_Valid_Userdata()
-        {
-            AuthenticationService service = new AuthenticationService(
-                MockHelpers.MockILogger<AuthenticationService>().Object,
-                _factory.Object,
-                _unitOfWork.Object
-            );
-            TokenDTO result = await service.SignInAsync(
-                new BLL.DTOs.LoginDTO()
-                {
-                    Login = "bbbbc",
-                    Password = "HelloWord123@"
-                });
-            Assert.NotNull(result);
-        }
-
-        [Fact]
-        public async Task AuthService_Should_Not_Authorize_With_Invalid_Userdata()
-        {
-            AuthenticationService service = new AuthenticationService(
-                MockHelpers.MockILogger<AuthenticationService>().Object,
-                _factory.Object,
-                _unitOfWork.Object
-            );
-            TokenDTO result = await service.SignInAsync(
-                new BLL.DTOs.LoginDTO()
-                {
-                    Login = "bbbbc",
-                    Password = "HellsssssoWord123@"
-                });
-            Assert.Null(result);
-        }
-
-        [Fact]
-        public async Task AuthService_Should_Refresh_Token()
-        {
-            AuthenticationService service = new AuthenticationService(
-                MockHelpers.MockILogger<AuthenticationService>().Object,
-                _factory.Object,
-                _unitOfWork.Object
-            );
-            TokenDTO result = await service.SignInAsync(
-                new BLL.DTOs.LoginDTO()
-                {
-                    Login = "bbbbc",
-                    Password = "HelloWord123@"
-                });
-
-            TokenDTO refreshResult = await service.TokenAsync(result);
-
-            Assert.NotNull(refreshResult);
         }
     }
 }
