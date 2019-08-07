6a06287efa22abd43a55903bca75918bde1c5868
Sviatoslav Bachynskyi <ba4insckij.s@gmail.com>
2019-08-07 16:38:27 +0300

    fixed bug with changing _localOptions

--- TransIT.DAL/FileStorage/LocalFileStorage.cs
+++ TransIT.DAL/FileStorage/LocalFileStorage.cs
@@ -17,13 +17,13 @@ namespace TransIT.DAL.FileStorage
         public string Create(IFormFile file)
         {
             Directory.CreateDirectory(_localOptions.FolderPath);
-            _localOptions.FolderPath = Path.Combine(_localOptions.FolderPath, DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss") + file.FileName);
+            var documentPath = Path.Combine(_localOptions.FolderPath, DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss") + file.FileName);
 
-            using (FileStream fileStream = new FileStream(_localOptions.FolderPath, FileMode.CreateNew))
+            using (FileStream fileStream = new FileStream(documentPath, FileMode.CreateNew))
             {
                  file.CopyTo(fileStream);
             }
-            return _localOptions.FolderPath;
+            return documentPath;
         }
 
         public void Delete(string FilePath)
