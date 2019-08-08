fd6ed4e3dc8eb35222d31468aa03ddb23cb9ddfb
Bohdan <44298130+BohdanBuhrii@users.noreply.github.com>
2019-08-08 11:43:11 +0300

    Fix bug with statistics (#37)

--- TransIT.API/Controllers/StatisticsController.cs
+++ TransIT.API/Controllers/StatisticsController.cs
@@ -19,7 +19,7 @@ namespace TransIT.API.Controllers
         }
 
         [HttpGet]
-
+        [Route("api/v1/[controller]/countmalfunction")]
         public async Task<IActionResult> CountMalfunction(string malfunctionName, string vehicleTypeName)
         {
             var result = await _statisticsService.CountMalfunction(malfunctionName, vehicleTypeName);
@@ -32,7 +32,7 @@ namespace TransIT.API.Controllers
         }
 
         [HttpGet]
-
+        [Route("api/v1/[controller]/countmalfunctionsubgroup")]
         public async Task<IActionResult> CountMalfunctionSubgroup(string malfunctionSubgroupName, string vehicleTypeName)
         {
             var result = await _statisticsService.CountMalfunctionSubGroup(malfunctionSubgroupName, vehicleTypeName);
@@ -45,7 +45,7 @@ namespace TransIT.API.Controllers
         }
 
         [HttpGet]
-
+        [Route("api/v1/[controller]/countmalfunctiongroup")]
         public async Task<IActionResult> CountMalfunctionGroup(string malfunctionGroupName, string vehicleTypeName)
         {
             var result = await _statisticsService.CountMalfunctionGroup(malfunctionGroupName, vehicleTypeName);
