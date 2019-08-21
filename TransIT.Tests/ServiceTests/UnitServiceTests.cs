e60656a012e08274ad02791f24a16a1a13c5df7f
Vlad <38084885+Vladacdc@users.noreply.github.com>
2019-08-21 17:41:27 +0300

    Added UnitServiceTests

--- TransIT.Tests/ServiceTests/UnitServiceTests.cs
+++ TransIT.Tests/ServiceTests/UnitServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Moq;
+using System;
 using System.Threading.Tasks;
 using TransIT.BLL.DTOs;
 using TransIT.BLL.Services.ImplementedServices;
@@ -22,6 +23,20 @@ namespace TransIT.Tests.ServiceTests
         }
 
         [Fact]
+        public async Task UnitService_Should_Get_Null_When_Unit_Not_Exist()
+        {
+            // Arrange
+            SetUpUnitOfWork();
+            var service = new UnitService(_unitOfWork.Object, _mapper);
+
+            // Act
+            var entity = await service.GetAsync((new Random()).Next());
+
+            // Assert
+            Assert.Null(entity);
+        }
+
+        [Fact]
         public async Task UnitService_Should_Create_Unit_Async()
         {
             // Arrange
@@ -41,6 +56,62 @@ namespace TransIT.Tests.ServiceTests
             Assert.NotNull(actualEntity);
             Assert.Equal(expectedEntity.Name, actualEntity.Name);
             Assert.Equal(expectedEntity.ShortName, actualEntity.ShortName);
+        }
+
+        [Fact]
+        public async Task UnitService_Should_Update_Unit()
+        {
+            // Arrange
+            SetUpUnitOfWork();
+            var service = new UnitService(_unitOfWork.Object, _mapper);
+            var entity = new UnitDTO()
+            {
+                Name = "TestName",
+                ShortName = "TestSN",
+            };
+
+            var newEntity = new UnitDTO()
+            {
+                Name = "NewTestName",
+                ShortName = "NewTestSN",
+            };
+
+            await service.CreateAsync(entity);
+            await _unitOfWork.Object.SaveAsync();
+
+            // Act
+            entity.Name = newEntity.Name;
+            entity.ShortName = newEntity.ShortName;
+            await service.UpdateAsync(entity);
+            await _unitOfWork.Object.SaveAsync();
+
+            // Assert
+            Assert.NotNull(entity);
+            Assert.Equal(newEntity.Name, entity.Name);
+            Assert.Equal(newEntity.ShortName, entity.ShortName);
+        }
+
+        [Fact]
+        public async Task UnitService_Should_Delete_Unit_Async()
+        {
+            // Arrange
+            SetUpUnitOfWork();
+            var service = new UnitService(_unitOfWork.Object, _mapper);
+            var entity = new UnitDTO()
+            {
+                Name = "TestName",
+                ShortName = "TestSN",
+            };
+
+            entity = await service.CreateAsync(entity);
+            await _unitOfWork.Object.SaveAsync();
+
+            // Act
+            await service.DeleteAsync(entity.Id);
+            await _unitOfWork.Object.SaveAsync();
+
+            // Assert
+            Assert.Null(await service.GetAsync(entity.Id));
         }
 
         private void SetUpUnitOfWork()
