29374cb303579c9435763255576f77c2353b41c1
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-08-21 19:02:32 +0300

    Add fixes for comments.

--- TransIT.BLL/Mappings/PartProfile.cs
+++ TransIT.BLL/Mappings/PartProfile.cs
@@ -8,7 +8,17 @@ namespace TransIT.BLL.Mappings
     {
         public PartProfile()
         {
-            CreateMap<PartDTO, Part>().ReverseMap();
+            CreateMap<PartDTO, Part>()
+                .ForMember(e => e.UpdatedById, opt => opt.Ignore())
+                .ForMember(e => e.CreatedById, opt => opt.Ignore())
+                .ForMember(e => e.UpdatedDate, opt => opt.Ignore())
+                .ForMember(e => e.CreatedDate, opt => opt.Ignore())
+                .ForMember(e => e.UnitId, opt => opt.MapFrom(e => e.Unit.Id))
+                .ForMember(e => e.ManufacturerId, opt => opt.MapFrom(e => e.Manufacturer.Id))
+                .ForMember(e => e.Unit, opt => opt.Ignore())
+                .ForMember(e => e.Manufacturer, opt => opt.Ignore());
+
+            CreateMap<Part, PartDTO>();
         }
     }
 }
