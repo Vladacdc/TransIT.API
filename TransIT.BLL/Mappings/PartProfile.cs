638654ab8ff946877847b316e607f2592b02660e
Vitalii Maksymiv <pbbbbc@gmail.com>
2019-08-22 15:44:43 +0300

    Fix 1 conflict

--- TransIT.BLL/Mappings/PartProfile.cs
+++ TransIT.BLL/Mappings/PartProfile.cs
@@ -8,9 +8,8 @@ namespace TransIT.BLL.Mappings
     {
         public PartProfile()
         {
-<<<<<<< HEAD
             CreateMap<PartDTO, Part>().ReverseMap();
-=======
+
             CreateMap<PartDTO, Part>()
                 .ForMember(e => e.UnitId, opt => opt.MapFrom(e => e.Unit.Id))
                 .ForMember(e => e.ManufacturerId, opt => opt.MapFrom(e => e.Manufacturer.Id))
@@ -18,7 +17,6 @@ namespace TransIT.BLL.Mappings
                 .ForMember(e => e.Manufacturer, opt => opt.Ignore());
 
             CreateMap<Part, PartDTO>();
->>>>>>> eb66ff9af4c4102c1e2251cc22beb91b11c7fdc2
         }
     }
 }
