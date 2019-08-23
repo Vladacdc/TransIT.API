fc0ac423d03f2c79f4b4aaefacfb2280b073940c
Vitalii Maksymiv <pbbbbc@gmail.com>
2019-08-23 17:13:51 +0300

    Rename collections

--- TransIT.DAL/Models/Entities/Part.cs
+++ TransIT.DAL/Models/Entities/Part.cs
@@ -21,7 +21,7 @@ namespace TransIT.DAL.Models.Entities
         public virtual User Create { get; set; }
         public virtual User Mod { get; set; }
 
-        public ICollection<IssueLogPart> IssueLogPart { get; set; }
-        public ICollection<SupplierPart> SupplierPart { get; set; }
+        public ICollection<IssueLogPart> IssueLogParts { get; set; }
+        public ICollection<SupplierPart> SupplierParts { get; set; }
     }
 }
